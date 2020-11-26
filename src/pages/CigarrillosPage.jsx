ebbafa590d1403db2b4190461db7af55dcb99afa
Martin <martinfernando-sandander@hotmail.com>
2020-11-26 20:56:47 -0300

    algo

--- src/pages/CigarrillosPage.jsx
+++ src/pages/CigarrillosPage.jsx
@@ -17,7 +17,7 @@ const CigarrillosPage = () => {
   }
 
   const formReset = () => {
-    dataMessage && setDataMessage({...dataMessage, nameClient: '', cellNumber: '', message: ''});
+    dataMessage && setDataMessage({ ...dataMessage, nameClient: '', cellNumber: '', message: '' });
   }
 
   const sendMessage = async (e) => {
@@ -35,18 +35,18 @@ const CigarrillosPage = () => {
   }
 
   const CardsCigarrillo = () => {
-    return(
+    return (
       dataCard.cigarros.map((c, i) =>
-    <Link onClick={() => modal(c, i)} className="col mb-4 text-dark" type="button" data-toggle="modal" data-target={`#modal${i}`}>
-      <div className="card h-100 ">
-        <img src={c.img.default} className="card-img-top img-fluid" alt={c.marca} />
-        <div className="card-body">
-          <h5 className="card-title">{c.marca}</h5>
-          <p className="card-text">{c.description}</p>
-        </div>
-      </div>
-    </Link>
-    ))
+        <Link onClick={() => modal(c, i)} className="col mb-4 text-dark" type="button" data-toggle="modal" data-target={`#modal${i}`}>
+          <div className="card h-100 ">
+            <img src={c.img.default} className="card-img-top img-fluid" alt={c.marca} />
+            <div className="card-body">
+              <h5 className="card-title">{c.marca}</h5>
+              <p className="card-text">{c.description}</p>
+            </div>
+          </div>
+        </Link>
+      ))
   }
 
   return (
@@ -56,80 +56,80 @@ const CigarrillosPage = () => {
           <CardsCigarrillo />
         </div>
         <div className="modal fade " id={`modal${modalDatos && modalDatos.indice}`} tabindice="-1" aria-hidden="true">
-        <div className="modal-dialog modal-dialog-centered modal-">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="staticBackdropLabel">{modalDatos && modalDatos.datos.marca}</h5>
-              <button type="button" className="close" onClick={formReset} data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div className="modal-body">
-              <img src={modalDatos && modalDatos.datos.img.default} className="card-img-top" alt="..." />
-              <div className="row pt-2">
-                <div className="col-12 col-sm-3 ">
-                  <div className="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                    <a className="nav-link active text-center" id={`v-pills-home-tab${modalDatos && modalDatos.indice}`} data-toggle="pill" data-target={`#v-pills-home${modalDatos && modalDatos.indice}`} role="tab" aria-controls={`v-pills-home${modalDatos && modalDatos.indice}`} aria-selected="true" >Info</a>
-                    <a className="nav-link text-center" id={`v-pills-profile-tab${modalDatos && modalDatos.indice}`} data-toggle="pill" data-target={`#v-pills-profile${modalDatos && modalDatos.indice}`} role="tab" aria-controls={`v-pills-profile${modalDatos && modalDatos.indice}`} aria-selected="false">Contacto</a>
-                  </div>
-                </div>
-
-                <div className="col-12 col-sm-9">
-                  <div className="tab-content" id="v-pills-tabContent">
-                    <div className="tab-pane fade show active" id={`v-pills-home${modalDatos && modalDatos.indice}`} role="tabpanel" aria-labelledby={`v-pills-home-tab${modalDatos && modalDatos.indice}`}>
-                      <p>{modalDatos && modalDatos.datos.descriptionModal}
-                      </p>
+          <div className="modal-dialog modal-dialog-centered modal-">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="staticBackdropLabel">{modalDatos && modalDatos.datos.marca}</h5>
+                <button type="button" className="close" onClick={formReset} data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <img src={modalDatos && modalDatos.datos.img.default} className="card-img-top" alt="..." />
+                <div className="row pt-2">
+                  <div className="col-12 col-sm-3 ">
+                    <div className="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                      <a className="nav-link active text-center" id={`v-pills-home-tab${modalDatos && modalDatos.indice}`} data-toggle="pill" data-target={`#v-pills-home${modalDatos && modalDatos.indice}`} role="tab" aria-controls={`v-pills-home${modalDatos && modalDatos.indice}`} aria-selected="true" >Info</a>
+                      <a className="nav-link text-center" id={`v-pills-profile-tab${modalDatos && modalDatos.indice}`} data-toggle="pill" data-target={`#v-pills-profile${modalDatos && modalDatos.indice}`} role="tab" aria-controls={`v-pills-profile${modalDatos && modalDatos.indice}`} aria-selected="false">Contacto</a>
                     </div>
+                  </div>
 
-                    <div className="tab-pane fade" id={`v-pills-profile${modalDatos && modalDatos.indice}`} role="tabpanel" aria-labelledby={`v-pills-profile-tab${modalDatos && modalDatos.indice}`}>
-                      <form className="pt-2" ref={formContact}>
-                        <div className="form-row">
-                          <div className="form-group col-md-6">
-                            <input
-                              type="text"
-                              name='nameClient'
-                              maxLength='32'
-                              value={dataMessage && dataMessage.nameClient}
-                              onChange={e => setDataMessage({...dataMessage, [e.target.name]: e.target.value})}
-                              id="nombre"
-                              className="form-control"
-                              placeholder='Nombre Completo' />
+                  <div className="col-12 col-sm-9">
+                    <div className="tab-content" id="v-pills-tabContent">
+                      <div className="tab-pane fade show active" id={`v-pills-home${modalDatos && modalDatos.indice}`} role="tabpanel" aria-labelledby={`v-pills-home-tab${modalDatos && modalDatos.indice}`}>
+                        <p>{modalDatos && modalDatos.datos.descriptionModal}
+                        </p>
+                      </div>
+
+                      <div className="tab-pane fade" id={`v-pills-profile${modalDatos && modalDatos.indice}`} role="tabpanel" aria-labelledby={`v-pills-profile-tab${modalDatos && modalDatos.indice}`}>
+                        <form className="pt-2" ref={formContact}>
+                          <div className="form-row">
+                            <div className="form-group col-md-6">
+                              <input
+                                type="text"
+                                name='nameClient'
+                                maxLength='32'
+                                value={dataMessage && dataMessage.nameClient}
+                                onChange={e => setDataMessage({ ...dataMessage, [e.target.name]: e.target.value })}
+                                id="nombre"
+                                className="form-control"
+                                placeholder='Nombre Completo' />
+                            </div>
+                            <div className="form-group col-md-6">
+                              <input
+                                type="number"
+                                className="form-control"
+                                id="cellphone"
+                                placeholder='Numero de teléfono'
+                                name="cellNumber"
+                                value={dataMessage && dataMessage.cellNumber}
+                                onChange={e => setDataMessage({ ...dataMessage, [e.target.name]: e.target.value })}
+                              />
+                            </div>
                           </div>
-                          <div className="form-group col-md-6">
-                            <input
-                              type="number"
+                          <div className="form-group">
+                            <textarea
                               className="form-control"
-                              id="cellphone"
-                              placeholder='Numero de teléfono'
-                              name="cellNumber"
-                              value={dataMessage && dataMessage.cellNumber}
-                              onChange={e => setDataMessage({...dataMessage, [e.target.name]: e.target.value})}
-                            />
+                              id="message-text"
+                              placeholder='Consulta:'
+                              name="message"
+                              value={dataMessage && dataMessage.message}
+                              onChange={e => setDataMessage({ ...dataMessage, [e.target.name]: e.target.value })}
+                            ></textarea>
                           </div>
-                        </div>
-                        <div className="form-group">
-                          <textarea
-                            className="form-control"
-                            id="message-text"
-                            placeholder='Consulta:'
-                            name="message"
-                            value={dataMessage && dataMessage.message}
-                            onChange={e => setDataMessage({...dataMessage, [e.target.name]: e.target.value})}
-                          ></textarea>
-                        </div>
-                      </form>
+                        </form>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={formReset}>Cerrar</button>
-              <button type="submit" className="btn btn-primary" onClick={sendMessage}>Enviar</button>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={formReset}>Cerrar</button>
+                <button type="submit" className="btn btn-primary" onClick={sendMessage}>Enviar</button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
       </div>
     </>
   )

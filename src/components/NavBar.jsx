dd0721f6dba499dbf1113fd33e685d2734492c79
Martin <martinfernando-sandander@hotmail.com>
2020-11-26 21:44:23 -0300

    quitando links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -17,13 +17,10 @@ const NavBar = (props) => {
         <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item active">
-              <Link className="nav-link text-white" to="/NorteTabacosFront">Inicio <span className="sr-only">(current)</span></Link>
+              <Link className="nav-link text-white" to="/NorteTabacosFront">Cigarrillos <span className="sr-only">(current)</span></Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link text-white" to="/fuegos-artificiales">Fuegos Artificiales</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link text-white" to="/">Nosotros</Link>
             </li>
           </ul>
         </div>
